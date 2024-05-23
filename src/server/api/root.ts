8367718261ecf0730e61b10b34ef808c0927b57a
shrprylz <158440736+shrprylz@users.noreply.github.com>
2024-05-23 09:11:24 +0000

    chore: add ignore eslint and typescript builds

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { carouselImageRouter } from '~/server/api/routers/carousel-image';
 import { contactRouter } from '~/server/api/routers/contact';
-import { postRouter } from '~/server/api/routers/post';
 import { productRouter } from '~/server/api/routers/product';
 import { serviceRouter } from '~/server/api/routers/service';
 import { createTRPCRouter } from '~/server/api/trpc';
@@ -11,7 +10,6 @@ import { createTRPCRouter } from '~/server/api/trpc';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   carouselImage: carouselImageRouter,
   service: serviceRouter,
   product: productRouter,
