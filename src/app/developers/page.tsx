27e470295aacffd34e909b7d3ba3e04012411308
shrprylz <158440736+shrprylz@users.noreply.github.com>
2024-06-01 07:24:59 +0000

    fix: developer page

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,10 +1,25 @@
+import { Github } from 'lucide-react';
 import { DeveloperCard } from '~/components/developer-card';
 
 export default function DevelopersPage() {
   return (
     <>
-      <div className="mx-auto max-w-lg py-4"></div>
-      <DeveloperCard />
+      <div className="mx-auto max-w-lg py-4">
+        <DeveloperCard
+          name="Justine N. Licuanan"
+          image=""
+          initials="JL"
+          title=""
+          socialLinks={[
+            {
+              Icon: Github,
+              link: 'https://github.com/JustineLicuanan',
+              className: 'bg-black hover:bg-black/80',
+            },
+          ]}
+        />
+        {/* <DeveloperCard name="" image="" initials="" title="" socialLinks={[{}]} /> */}
+      </div>
     </>
   );
 }
--- src/components/developer-card.tsx
+++ src/components/developer-card.tsx
@@ -1,25 +1,52 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '~/components/ui/card';
+import { LucideIcon } from 'lucide-react';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { buttonVariants } from '~/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { cn } from '~/lib/utils';
 
-export function DeveloperCard() {
+type SocialLink = {
+  link: string;
+  className?: string;
+  Icon: LucideIcon | JSX.ElementType;
+};
+
+type Props = {
+  name: string;
+  image: string;
+  initials: string;
+  title: string;
+  socialLinks?: SocialLink[];
+};
+
+export function DeveloperCard({ name, image, initials, title, socialLinks }: Props) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Developer</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
+    <Card className="flex items-center gap-4 p-4">
+      <Avatar className="h-24 w-24">
+        <AvatarImage src={image} />
+        <AvatarFallback>{initials}</AvatarFallback>
+      </Avatar>
+
+      <div>
+        <CardHeader>
+          <CardTitle>{name}</CardTitle>
+          <CardDescription>{title}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          {socialLinks?.map(({ Icon, link, className }, linkIdx) => (
+            <Link
+              key={linkIdx}
+              href={link}
+              className={cn(buttonVariants({ size: 'icon' }), 'rounded-full', className)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon />
+            </Link>
+          ))}
+        </CardFooter>
+      </div>
+      <svg />
     </Card>
   );
 }
