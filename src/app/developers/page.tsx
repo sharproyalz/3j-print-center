4ee8b4e0a3e284b8a5cf1d7946b7f988bdbec3c0
sharproyalz <116153953+sharproyalz@users.noreply.github.com>
2024-06-19 15:00:04 +0000

    chore: developer page

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,10 +1,17 @@
+import { Facebook } from 'lucide-react';
 import { DeveloperCard } from '~/components/developer-card';
 
 export default function DevelopersPage() {
   return (
     <>
       <div className="mx-auto max-w-lg py-4">
-        <DeveloperCard name="" image="" initials="" title="" socialLinks={[]} />
+        <DeveloperCard
+          name="Vonn Pactol"
+          image="/3J-logo.png"
+          initials="VP"
+          title="Web Developer"
+          socialLinks={[{ Icon: Facebook, link: 'https://www.messenger.com' }]}
+        />
       </div>
     </>
   );
